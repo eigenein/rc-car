7f6f8889f29376c875c26a2d3840c4503b232db4
Pavel Perestoronin <eigenein@gmail.com>
2017-10-13 14:24:21 +0200

    Add empty Android project

---
+++ android/app/src/main/kotlin/me/eigenein/arduinocar/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.eigenein.arduinocar
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
