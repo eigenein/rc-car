f860b5dcd8446aaba451db647a267d21e2d3f57e
Pavel Perestoronin <eigenein@gmail.com>
2017-11-05 18:14:31 +0100

    Tiny refactoring

--- android/app/src/main/java/me/eigenein/arduinocar/InputMessage.kt
+++ android/app/src/main/java/me/eigenein/arduinocar/InputMessage.kt
@@ -1,6 +1,20 @@
 package me.eigenein.arduinocar
 
+import java.io.InputStream
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
 interface InputMessage
 
 data class ConnectedMessage(val device_name: String) : InputMessage
 data class DeprecatedTelemetryMessage(val vcc: Float) : InputMessage
+
+fun InputStream.messageIterable() = object : Iterable<InputMessage> {
+    override fun iterator(): Iterator<InputMessage> = object : Iterator<InputMessage> {
+        override fun hasNext(): Boolean = true
+
+        override fun next(): InputMessage = DeprecatedTelemetryMessage(
+            ByteBuffer.wrap(readValue(6), 2, 4).order(ByteOrder.LITTLE_ENDIAN).int / 1000f
+        )
+    }
+}
